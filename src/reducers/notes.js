4c09aeea4fe9c668bb50de77d6ffc50f97f2ccb6
Risent Veber <risentveber@gmail.com>
2017-11-23 19:32:13 +0300

    add task for reducer

---
+++ src/reducers/notes.js
@@ -0,0 +1,12 @@
+export const ADD_NOTE = 'ADD_NOTE';
+export const REMOVE_NOTE = 'REMOVE_NOTE';
+export const REORDER_NOTE = 'REORDER_NOTE';
+export const UPDATE_NOTE = 'UPDATE_NOTE';
+export const DELETE_ALL_NOTES = 'DELETE_ALL_NOTES';
+
+export default function notes (state = [], action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
